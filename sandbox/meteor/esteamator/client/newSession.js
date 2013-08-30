abe1769ecd1bb09d1841f830cfb48506c03db0b7
Bruce Harris <bharris845@gmail.com>
2013-08-29 23:15:11 -0400

    formatting tweaks

--- sandbox/meteor/esteamator/client/newSession.js
+++ sandbox/meteor/esteamator/client/newSession.js
@@ -2,9 +2,9 @@ define('newSession', ['router', 'collections'], function(router, collections){
 
   Template.newSession.events({ 'click button': createNewSession });
 
-  function createNewSession(){
+  function createNewSession() {
     var sessionId = collections.sessions.insert({});
-    Session.set("sessionId", sessionId);
+    Session.set('sessionId', sessionId);
     router.setSession(sessionId);
     var itemId = collections.workItems.insert({
   	  sessionId: sessionId,
