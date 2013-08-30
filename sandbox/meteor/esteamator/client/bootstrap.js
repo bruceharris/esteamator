f5676edadcfbac0749ef155762c898e8c6c9571d
Bruce Harris <bharris845@gmail.com>
2013-08-29 23:14:39 -0400

    save user to sessionStorage
    as a user
    if I've already entered my username
    and I reload the page or try to join the session again in the same
    browser window
    I should not be re-prompted to enter my name
    and I should be presented with the item being estimated immediately

---
+++ sandbox/meteor/esteamator/client/bootstrap.js
@@ -0,0 +1,14 @@
+define('bootstrap', ['collections'], function(collections) {
+
+  Deps.autorun(setUserToSessionIfStoredLocally);
+
+  function setUserToSessionIfStoredLocally() {
+    if (!Session.equals('user', null)) return;
+    var localUserId = window.sessionStorage.getItem('userId');
+    var sessionId = Session.get('sessionId');
+    if (!localUserId || !sessionId) return;
+    var user = collections.users.findOne({ _id: localUserId, sessionId: sessionId });
+    if (user) Session.set('user', user);
+  }
+
+});
--- sandbox/meteor/esteamator/client/inputUsername.js
+++ sandbox/meteor/esteamator/client/inputUsername.js
@@ -2,7 +2,7 @@ define('inputUsername', ['collections'], function(collections) {
 
   Template.inputUsername.events({ 'change input': registerUsername });
 
-  function registerUsername(event, template){
+  function registerUsername(event, template) {
     var user = {
   	  sessionId: Session.get('sessionId'),
   	  name: $(template.find('input')).val()
@@ -10,6 +10,7 @@ define('inputUsername', ['collections'], function(collections) {
     var userId = collections.users.insert(user);
     user = _(user).extend({ _id: userId });
     Session.set('user', user);
+    window.sessionStorage.setItem('userId', userId);
   }
 
   return null;
