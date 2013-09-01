7caef0cb4432158eb503a45938459cb4c298562f
Bruce Harris <bharris845@gmail.com>
2013-09-01 13:40:24 -0400

    formatting tweaks

--- sandbox/meteor/esteamator/client/bootstrap.js
+++ sandbox/meteor/esteamator/client/bootstrap.js
@@ -4,9 +4,11 @@ define('bootstrap', ['collections'], function(collections) {
 
   function setUserToSessionIfStoredLocally() {
     if (!Session.equals('user', null)) return;
+
     var localUserId = window.sessionStorage.getItem('userId');
     var sessionId = Session.get('sessionId');
     if (!localUserId || !sessionId) return;
+
     var user = collections.users.findOne({ _id: localUserId, sessionId: sessionId });
     if (user) Session.set('user', user);
   }
