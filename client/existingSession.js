fb9d33164dffd159d88a8d595079932b2255e0d4
Bruce Harris <bharris845@gmail.com>
2013-09-01 18:07:33 -0400

    provide link to email session to team members

--- client/existingSession.js
+++ client/existingSession.js
@@ -1,3 +1,17 @@
-Template.existingSession.noUser = function () {
-  return Session.equals('user', null);
-};
+'use strict';
+
+Template.existingSession.helpers({
+
+  noUser: function() {
+    return Session.equals('user', null);
+  },
+
+  url: function() {
+    return window.location;
+  },
+
+  mailHref: function() {
+    return 'mailto:?subject=Esteamator%20Session&body=' + window.location;
+  }
+
+});
