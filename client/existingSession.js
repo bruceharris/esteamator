b561a33a1264d19010df5ffa98007bee9c824986
Bruce Harris <bharris845@gmail.com>
2013-09-01 18:34:38 -0400

    hide share link after 2+ users have joined

--- client/existingSession.js
+++ client/existingSession.js
@@ -1,9 +1,18 @@
-'use strict';
+define('existingSession', ['collections'], function(collections) {
+  'use strict';
 
-Template.existingSession.helpers({
+  Template.existingSession.helpers({
 
-  noUser: function() {
-    return Session.equals('user', null);
-  },
+    noUser: function() {
+      return Session.equals('user', null);
+    },
+
+    shouldShowFullShareLink: function() {
+      var query = { sessionId: Session.get('sessionId') };
+      var users = collections.users.find(query).fetch();
+      return users.length < 2;
+    }
+
+  });
 
 });
