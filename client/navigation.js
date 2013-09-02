f77c758417c3f05c2b4f4459ed95eb2ff9cdd394
Bruce Harris <bharris845@gmail.com>
2013-09-02 15:29:54 -0400

    on report page - show link to resume estimating

--- client/navigation.js
+++ client/navigation.js
@@ -8,10 +8,17 @@ define('navigation',
     return estimateHelpers.workItemsInSession().length > 1;
   }
 
+  function sessionHref() {
+    return '/session/' + Session.get('sessionId');
+  }
+
+  Template.navigation.helpers(pageHelpers);
   Template.navigation.helpers({
 
+    sessionHref: sessionHref,
+
     reportHref: function() {
-      return '/session/' + Session.get('sessionId') + '/report';
+      return sessionHref() + '/report';
     },
 
     shouldShowReportLink: function() {
