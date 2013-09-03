9a493bcb4c329662d5d5008a70fe2f1b3d730e9e
Bruce Harris <bharris845@gmail.com>
2013-09-03 09:55:07 -0400

    DRY up publish.js

--- server/publish.js
+++ server/publish.js
@@ -1,30 +1,15 @@
 define('publish', ['collections'], function(collections) {
   'use strict';
 
-  function query(sessionId) {
-    return { sessionId: sessionId };
+  function publishForReqestedSessionId(collection) {
+    Meteor.publish(collection, function(sessionId) {
+      return collections[collection].find({ sessionId: sessionId });
+    });
   }
 
-  // Publish all work items for requested sessionId
-  Meteor.publish('workItems', function(sessionId) {
-    var result = collections.workItems.find(query(sessionId));
-    console.log('publishing work items', result.count(), result.fetch());
-    return result;
-  });
-
-  // Publish all users for requested sessionId
-  Meteor.publish('users', function(sessionId) {
-    var result = collections.users.find(query(sessionId));
-    console.log('publishing users', result.count(), result.fetch());
-    return result;
-  });
-
-  // Publish all estimates for requested sessionId
-  Meteor.publish('estimates', function(sessionId) {
-    var result = collections.estimates.find(query(sessionId));
-    console.log('publishing estimates', result.count(), result.fetch());
-    return result;
-  });
+  publishForReqestedSessionId('workItems');
+  publishForReqestedSessionId('users');
+  publishForReqestedSessionId('estimates');
 
   return null;
 
