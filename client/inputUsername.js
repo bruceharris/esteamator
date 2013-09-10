c636faf17296b0ecacfa8d0e2711602785fe81a2
Bruce Harris <bharris845@gmail.com>
2013-09-09 22:48:16 -0400

    client side support for username input validation
    only save user to session if insert succeeded on server

--- client/inputUsername.js
+++ client/inputUsername.js
@@ -4,14 +4,24 @@ define('inputUsername', ['collections'], function(collections) {
   Template.inputUsername.events({ 'change input': registerUsername });
 
   function registerUsername(event, template) {
+
     var user = {
   	  sessionId: Session.get('sessionId'),
-  	  name: $(template.find('input')).val()
+  	  name: template.find('input').value
     };
-    var userId = collections.users.insert(user);
-    user = _(user).extend({ _id: userId });
-    Session.set('user', user);
-    window.sessionStorage.setItem('userId', userId);
+
+    collections.users.insert(user, uponInsert);
+
+    function uponInsert(err, id) {
+      if (err) {
+        // TODO graceful error handling
+        return;
+      }
+      user = _(user).extend({ _id: id });
+      Session.set('user', user);
+      window.sessionStorage.setItem('userId', id);
+    }
+
   }
 
   return null;
